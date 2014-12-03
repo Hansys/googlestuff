17a28c3fc4050cfeb5562b0ee771841db8334bf3
Hansys <hans.balis@gmail.com>
2014-12-03 19:34:49 +0100

    Initiele commit

---
+++ controllers/my_controllers.py
@@ -0,0 +1,8 @@
+import openerp.http as http
+from openerp.http import request
+
+class MyController(http.Controller):
+
+    @http.route('/google0de4eeccbb910ddf.html', type="http")
+    def google0de4eeccbb910ddf(self):
+        return "google-site-verification: google0de4eeccbb910ddf.html"
